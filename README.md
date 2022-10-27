f3731bbc6a316dceff2aa7bb6e044242b152bbea
Anil Dhiman <anil.dhiman@digital.justice.gov.uk>
2022-10-27 17:25:11 +0100

    Converted to unit test with no assertion.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # 2i-code-test
 
-Run the Main class from CLI
-
-  mvn exec:java -Dexec.mainClass="com.ad.code.Main"
+- Run the Main class from CLI
+  - `mvn test`
   
 
-Run the Main from IDE as usual.
+- Run the Main from IDE as usual.
+  - open the project as `Maven Project` in your choice of IDE, I used intelliJ
+  - open `src/test/com/ad/code/Tests`
+  - execute the method `testUniqueReverseList()` or class `Tests` as its annotated by junit test `@Test`
+  
--- src/test/com/ad/code/Tests.java
+++ src/test/com/ad/code/Tests.java
@@ -2,14 +2,16 @@ package com.ad.code;
 
 import java.util.List;
 import java.util.Set;
+import org.junit.Test;
 
 import static com.ad.code.CodeTestChallenge.createRawListOfNumbers;
 import static com.ad.code.CodeTestChallenge.reverseSortedList;
 import static com.ad.code.CodeTestChallenge.uniqueSortedList;
 
-public class Main {
-    public static void main(String[] args) {
+public class Tests {
 
+    @Test
+    public void testUniqueReverseList() {
         List<Integer> rawUnsortedListOfNumbers = createRawListOfNumbers();
         List<Integer> reverseListOfNumbers = reverseSortedList(rawUnsortedListOfNumbers);
         Set<Integer> uniqueReverseListOfNumbers = uniqueSortedList(reverseListOfNumbers);
